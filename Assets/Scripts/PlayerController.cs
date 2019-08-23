86650905c6fdc9a452a64055f11c6de0a1cd4098
Jelle-M <jelle.meeus@hotmail.com>
2019-08-23 19:23:34 +0200

    Add animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     [SerializeField]
     private float speed = 1;
     private Rigidbody2D rb2d;
@@ -17,8 +18,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        movement = movement.normalized;
+        movement = (new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))).normalized;
+        setAnimator(movement);
     }
 
     void FixedUpdate()
@@ -29,5 +30,53 @@ public class PlayerController : MonoBehaviour
     void moveCharacter(Vector2 direction)
     {
         rb2d.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    void setAnimator(Vector2 direction)
+    {
+        float horizontal = movement[0];
+        float vertical = movement[1];
+        Debug.LogFormat("H {0} V {1}", horizontal, vertical);
+        animator.SetFloat("speed", horizontal*horizontal + vertical*vertical);
+        if (horizontal < 0)
+        {
+            animator.SetInteger("direction", 2);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger("direction", 3);
+        }
+        else if (vertical > 0)
+        {
+            animator.SetInteger("direction", 16);
+        }
+        else
+        {
+            animator.SetInteger("direction", 0);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        switch(col.gameObject.tag)
+        {
+            case "BombPowerUp":
+                Debug.Log("BombPowerUp!");
+                break;
+            case "SpeedPowerUp":
+                Debug.Log("SpeedPowerUp!");
+                break;
+            case "ShieldPowerUp":
+                Debug.Log("ShieldPowerUp!");
+                break;
+            case "BlastPowerUp":
+                Debug.Log("BlastPowerUp!");
+                break;
+            case "DeadZone":
+                Debug.Log("DeadZone!");
+                break;
+            default:
+                break;
+        }
     }
 }
