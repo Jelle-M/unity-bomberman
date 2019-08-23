3e74a95bba569cdf3e675b47ba4f2ddc26ea34fc
Jelle-M <jelle.meeus@hotmail.com>
2019-08-23 15:49:30 +0200

    Add movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1;
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb2d.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+}
