14f584db97e5119f5e0d8b0281ece215507c5a99
Jelle-M <jelle.meeus@hotmail.com>
2019-08-23 21:13:31 +0200

    Add Bomb spawning and destroying

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float countdown = 3f;
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f)
+        {
+            // explode
+            Debug.Log("Explode!");
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombSpawner : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public GameObject bomb;
+    private int n_bombs = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && n_bombs > 0)
+        {
+            Vector3Int cellPosition = tilemap.WorldToCell(transform.position);
+            Debug.Log(transform.position);
+            Vector3 centerPosition = tilemap.GetCellCenterWorld(cellPosition);
+            Instantiate(bomb, centerPosition, Quaternion.identity);
+            n_bombs--;
+            Invoke("addBomb", 1); //RemoveLater
+        }
+        
+    }
+
+    void addBomb()
+    {
+        n_bombs++;
+    }
+}
